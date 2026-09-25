0bdf1e23b8141cefa7917e2005058bd59cf2cb9e
Deeksha Gowda <deekshagowda366@gmail.com>
2026-09-25 17:26:12 +0530

    Delete server directory

--- server/src/routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getAnalyticsSummary, trackResumeDownload, trackVisit } from '../controllers/analyticsController.js';
-
-export const analyticsRouter = Router();
-
-analyticsRouter.post('/visit', trackVisit);
-analyticsRouter.post('/resume-download', trackResumeDownload);
-analyticsRouter.get('/summary', getAnalyticsSummary);
--- server/src/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createContactMessage } from '../controllers/contactController.js';
-import { contactSpamProtection } from '../middleware/spamProtection.js';
-import { validateContact } from '../middleware/validate.js';
-
-export const contactRouter = Router();
-
-contactRouter.post('/', contactSpamProtection, validateContact, createContactMessage);
